771cb85b28a00ce32a69f7e603bc208cd842fac6
Andrew Kang <andrew716@gmail.com>
2018-11-28 03:11:56 -0800

    error test

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -6,6 +6,8 @@ it('expect Jack Doe', async () => {
   const handler = require('../handler')
   const payload = await handler.helloworld({ name })
 
+  throw new Error('FakeErr')
+
   expect(payload).toBeDefined()
   expect(payload).toContain(name)
 });
