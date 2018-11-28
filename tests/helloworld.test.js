0c3f9749ae5ff173eb6ad38b65ba9bffecd0cbd6
Andrew Kang <andrew716@gmail.com>
2018-11-28 02:44:55 -0800

    debug...

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -1,20 +1,10 @@
-const AWS = require('aws-sdk')
-const lambda = new AWS.Lambda()
-
-// const lambda = new AWS.Lambda({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_KEY,
-//   region: process.env.AWS_REGION
-// });
 
 it('expect Jack Doe', async () => {
 
   const name = 'Jack Doe'
 
-  const payload = await lambda.invoke({
-    FunctionName: 'serverless-ci-dev-helloworld',
-    Payload: JSON.stringify({ name })
-  }).promise().then(resp => resp.Payload)
+  const handler = require('../handler')
+  const payload = await handler.helloworld({ name })
 
   expect(payload).toBeDefined()
   expect(payload).toContain(name)
