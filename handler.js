984fc6b64dc6fc737d5ebb145d49ddf229a6661a
Andrew Kang <andrew716@gmail.com>
2018-11-28 00:11:34 -0800

    build command

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
+const moment = require('moment')
+
 exports.helloworld = async (event) => {
   const { name } = event
+  const m = moment()
 
-  return `hello world ${name}`
+  return `hello world from ${name || '???'} at ${m.format('YYYY-MM-DD HH:mm:ss')}`
 }
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    minimize: false
+  },
+  performance: {
+    hints: false
+  },
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  }
+};
