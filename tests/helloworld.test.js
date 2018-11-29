22c6cd470f83b69888f83bdb8c7d241e510b948c
Andrew Kang <andrew.k@innovativemetrics.com>
2018-11-29 03:28:43 +0000

    should success

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -6,7 +6,7 @@ it('expect Jack Doe', async () => {
   const handler = require('../handler')
   const payload = await handler.helloworld({ name })
 
-  throw new Error('FakeErr')
+  // throw new Error('FakeErr')
 
   expect(payload).toBeDefined()
   expect(payload).toContain(name)
