6e765ed72a3033274286c4bd117aa6c26cfce69a
Andrew Kang <andrew716@gmail.com>
2018-12-01 00:52:02 -0800

    remove nested

--- nested/handler.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment')
-
-exports.helloworld = async (event) => {
-  const { name } = event
-  const m = moment()
-
-  return `hello world from ${name || '???'} at ${m.format('YYYY-MM-DD HH:mm:ss')}`
-}
